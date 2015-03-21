9949724e9bcb61d7e92360907c4566a09b55a0b6
Kuba Wasylkow <kuba_160@o2.pl>
2015-03-21 21:23:36 +0100

    last commit for today (2015-03-21)

--- generatelist.php
+++ generatelist.php
@@ -10,5 +10,24 @@
 		echo "Directory is empty! Returning to [INSERT SHELL NAME HERE].";
 		exit("\n");
 	}
-	var_dump($array);
+	// returns when doc is empty
+
+	$dirsum = 0;
+	$filesum = 0;
+	// 0ing
+
+	foreach($array as $i => $i_value) {
+		if(filetype($directory.'/'.$array[$i]) == "dir"){
+			$dirsum++;
+		}
+                else {
+                        $filesum++;
+		}
+	}
+	// count foles and dirs
+	echo "Found ", $dirsum, " folders and ", $filesum, " files.";
+
+
+
+//	var_dump($array);
 ?>
