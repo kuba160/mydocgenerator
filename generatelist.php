d21f27250ef8cd2f806d64b35629e55b34bf5394
Kuba Wasylkow <kuba_160@o2.pl>
2015-03-30 17:01:35 +0200

    1st working version

--- generatelist.php
+++ generatelist.php
@@ -4,8 +4,17 @@
 	//
 	// Here are the constants
 	$dirsum = 0;
-	$filesum = 1;
+	$filesum = 0;
+	$ignorefile = "ignore";
+	$settingsfile = "settings";
+	$ignored = 0;
+	#settings = 0;
 	//
+	//File list
+	$fulllist = array();
+	$listfiles = array();
+	$listdirs = array();
+	$settingfolder;
 	// Here are the variables
 	//
 	// Directory of documentation (DON'T USE ./)
@@ -22,22 +31,41 @@
 	}
 	//
 	// Now cout all dirs and files to dir- and filesum
+	// Make a filelist
 	foreach($array as $i => $i_value) {
-		if(filetype($directory.'/'.$array[$i]) == "dir"){
-			$dirsum++;
-		}
-                else {
-                        $filesum++;
+		switch(filetype($directory.'/'.$array[$i])) {
+			case "dir":
+				$dirsum++;
+				if(file_exists($directory.'/'.$array[$i].'/'.$ignorefile)){
+					$ignored++;
+					break;
+				}
+				if(file_exists($directory.'/'.$array[$i].'/'.$settingsfile)){
+					$settingfolder = $array[$i];
+					$listdirs[] = array($array[$i] => "settings");
+					break;
+				}
+				$listdirs[] = array($array[$i] => "dir");
+				break;
+			case "file":
+		                $filesum++;
+				$listfiles[] = array($array[$i] => "file");
+				break;
 		}
 	}
 	//
 	// Return how many files and folders are found
-	echo "Found ", $dirsum, " folders and ", $filesum, " files.\n";
-
+	echo "Found ", $dirsum, " folders and ", $filesum, " files.\n$ignored folder(s) was/were ignored because of ignorefile.\n";
+	//
+	//
+	echo "Folder \"", $settingfolder, "\" is used for settings.\n";
 	// To be continued...
-
-
+	//Makes a filelist
+	$list = array_merge($listdirs, $listfiles);
+	// 	Returns the encoded array in json
+	//	echo(json_encode($array));
 	//	Returns the array of directory. Used only for bug-checking
 	//	var_dump($array);
-	//
+	//	echo(json_encode($list));
+		var_dump($list);
 ?>
