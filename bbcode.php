b6b0706933d09f4fa59a6fa939c60b3272b256df
Jakub Wasylkow <kuba_160@o2.pl>
2015-04-03 14:53:19 +0200

    commit

--- bbcode.php
+++ bbcode.php
@@ -4,10 +4,19 @@
 	$searchfor = array("[b]","[/b]");
 	$replaceto = array("<b>","<b>");
 
+	$pathfrom = "./doc/dir/a";
+	$pathto = "./generated/dir/a";
+
+	replacefile($pathfrom,$pathto);
+
 	function replacefile($pathfrom,$pathto){
-		$file = fopen($pathfrom,"r");
-		$fcontent = file_get_contents($file, false);
+		//$file = fopen($pathfrom,"r");
+		$fcontent = file_get_contents($pathfrom, false);
 		$replaced = str_replace($searchfor, $replaceto, $fcontent, $replaced);
-		return $replaced;
+		//fclose($file);
+
+		$file = fopen($pathto,"w");
+		fwrite( $file,$replaced);
+		fclose($file);
+		return 0;
 	}
-		
