1aecf57f151ebb9d9b4871377f951ae08cf9d5a9
Jakub Wasylkow <kuba_160@o2.pl>
2015-04-07 21:41:26 +0200

    prepare.php changes

--- fulllist.php
+++ fulllist.php
@@ -10,51 +10,49 @@
 	// functions?
 	// ??? make an array from folder ???
 
-	$dirarray = array();
-	$filearray = array();
-	$variablefile = "variables.json";
-	// OK, time to get variables
-	// Now get the content
-	if( file_exists("./variables.json"))
-		extract( json_decode(file_get_contents($variablefile), true), EXTR_OVERWRITE);
-	else
-		exit("We don't have variables.json!\nHave you ran prepare.php first?\n");
 
-// Now
+	// First get the path ($directory.$settingsdir)
+	$directory = file_get_contents(".continue");
+
+	// Now get the variables
+	extract( json_decode(file_get_contents($directory.'/'."variables.json"), true), EXTR_OVERWRITE);
 
 
-	// Now we (should) have $directory $ignoredir and $settingsdir variables
-	// $variablefile $statfile $listfile
+	// Get custom variables
+	if( file_exists("./vars.json"))
+		extract( json_decode(file_get_contents("./vars.json")), EXTR_OVERVRITE);
+
+
+	// We got:
+	// $directory,$ignoredir,$settingsdir,$variablefile,$listfile,$statfile,$continuefile
+
+	//	Make statfilepath (it should be empty)
 	$statfilepath = $directory.'/'.$settingsdir.'/'.$statfile;
-	if( file_exists($statfilepath))
-		extract( json_decode(file_get_contents($statfilepath), true), EXTR_OVERWRITE);
-	else
-		exit("We don't have stats.json!\nHave you ran prepare.php first?\n");
 	// $dirsum $diffsum $filesum
 
 	// Now we need to get a filelist
-	$array = json_decode(file_get_contents($directory.'/'.$settingsdir.'/'."list.json"), true);
+//	$array =  array_diff(scandir($directory), array('..', '.'));
 //var_dump($array);
-	// Now each root dir
-/*
-	foreach ($array as $i => $i_value) {
-		switch(filetype($directory.'/'.$array[$i])) {
-			case "dir":
-					$dirarray[] = GetArrayFromDir($directory, $array[$i]);
-					break;
-			case "file":
-					$filearray[] = $array[$i];
-					break;
-	}
-}
-*/
-global $settingsdir;
-$fulllist = array_filter(GetArrayFromDir($directory));
+
+	$dirsum = 0;
+	$filesum = 0;
+//global $settingsdir;
+
+$fulllist = GetArrayFromDir($directory);
 var_dump($fulllist);
+
+
+foreach ($fulllist["dir"] as $i => $i_value) {
+	echo($i." - ");
+	echo($i_value."\n");
+}
+
 //echo(json_encode($fulllist));
 
 	function GetArrayFromDir($path){
 	global $settingsdir;
+	global $dirsum;
+	global $filesum; 
 	$array = array_diff(scandir($path), array('..', '.'));
 	$filearray = array();
 	$dirarray = array();
@@ -63,15 +61,15 @@ var_dump($fulllist);
 			case "dir":							// When dir
 				if($array[$i] == $settingsdir)
 					break;
-//				$dirsum++;
+				$dirsum++;
 				$dirarray[$array[$i]] = GetArrayFromDir($path.'/'.$array[$i]);
 				break;
 			case "file":							// When file
-//		    	$filesum++;					// sum it
+		    	$filesum++;					// sum it
 				$filearray[] = $array[$i];// = $array[$i];	// add it to array
 				break;							//
 		}
 	}
-	return array_merge($dirarray, $filearray);
+	return /*array( "list" => */array_merge($dirarray, $filearray);//, "dirsum" => $dirsum, "filesum" => $filesum);
 	}
 ?>
