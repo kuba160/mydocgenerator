8b0656e6a1304e0ae3d96dc34db318f9b20a5807
Kuba Wasylkow <kuba_160@o2.pl>
2015-04-19 20:10:47 +0200

    working...

--- bbcode.php
+++ bbcode.php
@@ -4,28 +4,39 @@
 
 	// For first change b,i,u,s
 
-function bbcode_arrays(){
-global $searchfor = array(	"[b]","[/b]",
-				"[i]"."[/i]",
-				"[u]","[/u]",
-				"[s]","[/s]"
-				);
+function bbcode_arrays($searchfor,$replaceto){
+$searchfor = array(	"[b]"		,	"[/b]",
+							"[i]"		,	"[/i]",
+							"[u]"		,	"[/u]",
+							"[s]"		,	"[/s]",
+							"[url]"		,	"[/url]",
+							"[red]"		, 	"[/red]",
+							"[green]"	,	"[/green]",
+							);
 
-global $replaceto = array(	"<b>","</b>",
-				"<i>","</i>",
-				"<u>","</u>",
-				"<s>","</s>"
-				);
-
+ $replaceto = array(	"<b>"				,	"</b>",
+							"<i>"				,	"</i>",
+							"<u>"				,	"</u>",
+							"<s>"				,	"</s>",
+							"<a href=\""		,	"\"/>",
+							"<font color=\"red\">",	"</font>"
+							);
+return 0;
 }
+
+
+	$searchfor = array();
+	$replaceto = array();
 	// These are used for testing now
 	$pathfrom = "./doc/dir/a";
 	$pathto = "./generated/dir/a";
 	$name = "my.function";
-//	bbcode_arrays();
-//	bbcode_replace($pathfrom,$pathto);
+	bbcode_arrays($searchfor,$replaceto);
+	bbcode_replace($pathfrom,$pathto, $name, $searchfor,$replaceto);
 
-	function bbcode_replace($pathfrom,$pathto, $name){
+
+
+	function bbcode_replace($pathfrom,$pathto, $name,$searchfor,$replaceto){
 		// First prepare the data
 		//$file = fopen($pathfrom,"r");
 		$fcontent = file_get_contents($pathfrom, false);
@@ -34,6 +45,7 @@ global $replaceto = array(	"<b>","</b>",
 
 		// Now we can start the file
 		// First init file
+		touch($pathto);
 		$file = fopen($pathto,"w");
 
 
