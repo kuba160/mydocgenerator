5cf572cb5e44db167d27a94b1fa0b0742255fe5b
Jakub Wasylkow <kuba@debian>
2015-05-26 15:01:18 +0200

    started writing

--- sitepages.php
+++ sitepages.php
@@ -3,3 +3,8 @@
 	// It needs a list with all bbcode files
 	// Every file on that list will be transformed into html in other folder
 
+	$settingspath = file_get_contents(".continue");
+	// Get the variables
+	extract( json_decode(file_get_contents($settingspath.'/'."variables.json"), true), EXTR_OVERWRITE);
+
+	// Get the array
